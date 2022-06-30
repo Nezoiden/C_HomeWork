d0c546ecded71607486e1d8f8838470fbfe4ce63
Sergey <xo4ywka@yandex.ru>
2022-06-30 16:56:32 +0300

    Доработал 07 задачу

--- Task_07/Program.cs
+++ Task_07/Program.cs
@@ -11,11 +11,13 @@ int num;
 while (true)
 {
     num = Convert.ToInt32(Console.ReadLine());
+    num = Math.Abs(-num);
     if (num >= 100 && num < 1000)
     {
         break;
     }
     Console.WriteLine($"Число {num} не является трёхзначным, введите другое: ");
+
 }
-int num1 = num % 10 ;
+int num1 = num % 10;
 Console.WriteLine($"Последняя цифра Вашего числа: {num1}");
