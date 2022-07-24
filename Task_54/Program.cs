75421ad5f1c1ff86d66b3bbe58c737406c55e7f4
Sergey <xo4ywka@yandex.ru>
2022-07-24 15:50:22 +0300

    Задача 54

---
+++ Task_54/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит
+//  по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    int[,] arr = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < arr.GetLength(1) - 1) Console.Write($"{arr[i, j],3} | ");
+            else Console.Write($"{arr[i, j],3} ] ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortNumbers(int[,] arr)
+{
+   
+    for (int i = 0; i < arr.GetLength(0); i++) 
+    {
+        for (int j = arr.GetLength(1) - 1; j > 0; j--)
+        {
+
+            for (int k = 0; k < j; k++)
+            {
+                if (arr[i, k] < arr[i, k + 1])
+                {
+                    int temp = arr[i, k];
+                    arr[i, k] = arr[i, k + 1];
+                    arr[i, k + 1] = temp;
+                }
+            }
+        }
+        
+        }
+}  
+    
+
+
+int[,] arrayResult = CreateMatrixRndInt(4, 4, 1, 100);
+PrintMatrix(arrayResult);
+Console.WriteLine();
+SortNumbers(arrayResult);
+PrintMatrix(arrayResult);
+
